03b4d828f80c61af69b4b5b4a9ed01c72b6a3482
root <bharadwajrembarr@gmail.com>
2016-12-21 06:56:07 +0000

    #3

--- test/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class test{
-  file {'/home/admin/puppet_render.txt':
-    ensure => present,
-    content => "This was created by Puppet using test module",
-    owner   => root,
-    group   => root,
-    mode    => 0644,
-  }
-}
